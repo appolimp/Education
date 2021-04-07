d496526e119006afbdf1a94132f1ee8eb75c9fb5
appolimp <appolimp@mail.ru>
2021-04-07 11:38:44 +0300

    Update eml2.py
    refactor

--- Work/eml/eml2.py
+++ Work/eml/eml2.py
@@ -5,11 +5,40 @@ import email.parser
 import email.policy
 import datetime
 import shutil
+import unicodedata
+import re
 
 PATH = 'data\\'
 PATH_OUT = 'out'
 PATH_IMAGE = 'img'
 PATH_ATTACH = 'attach'
+
+
+# ###################### Folder and Files ###################################
+def make_file_name_valid(file_name, repl='', empty='empty_name'):
+    """
+
+    :type file_name: str
+    :param repl: На какой символ заменять
+    :type repl: str
+    :param empty: Значение если имя пустое
+    :type empty: str
+    :rtype: str
+    """
+
+    name = ''.join(ch for ch in file_name if unicodedata.category(ch)[0] != 'C')  # Remove control chars
+    name = re.sub(r'[/\\?%*:|<>]', repl, name)  # remove non valid chars
+    name = name.strip()  # remove space in start and end
+
+    while name.endswith('.'):
+        name = name[:-1]
+
+    name = name if name else empty
+
+    if name != file_name:
+        logging.debug('Make valid name: ["{}" --> "{}"]'.format(file_name, name))
+
+    return name
 
 
 def create_name_for_folder(data_str, subject):
@@ -23,12 +52,56 @@ def create_name_for_folder(data_str, subject):
 
     date = datetime.datetime.strptime(data_str, '%a, %d %b %Y %X %z')
     name = '{}_{}'.format(date.date(), subject)
+    valid_name = make_file_name_valid(name)
 
-    logging.debug('Create name for folder: "{}"'.format(name))
-    return name
+    logging.debug('Create name for folder: "{}"'.format(valid_name))
+    return valid_name
 
 
-def parsing_file(eml_path):
+def create_folder_or_pass_if_created(folder_path):
+    try:
+        os.mkdir(folder_path)
+        logging.debug('Create folder "{}"'.format(folder_path))
+    except FileExistsError:
+        pass
+
+
+def get_or_create_folder_for_save(file_path, out_folder_name, folder_name):
+    folder, _ = os.path.split(file_path)
+
+    folder_out = os.path.join(folder, out_folder_name)
+    create_folder_or_pass_if_created(folder_out)
+
+    folder_for_save = os.path.join(folder_out, folder_name)
+    create_folder_or_pass_if_created(folder_for_save)
+
+    logging.debug('Get or create folder for save for file: [{} --> {}]'.format(file_path, folder_for_save))
+    return folder_for_save
+
+
+def write_part(part, folder_for_save, folder_for_part):
+    file_name = make_file_name_valid(part.get_filename())
+
+    absolute_path = os.path.join(folder_for_part, file_name)
+    relative_path = os.path.relpath(absolute_path, folder_for_save)  # Для связи c html
+
+    with open(absolute_path, 'wb') as fb:
+        fb.write(part.get_payload(decode=True))
+
+    logging.debug('File "{}" was recorded'.format(file_name))
+    return file_name, relative_path
+
+
+def write_html(html, folder_path, name):
+    html_path = os.path.join(folder_path, make_file_name_valid(name))
+    with open(html_path, 'wb') as f:
+        f.write(html)
+
+    return html_path
+
+
+# ###################### Parsing ############################################
+def get_email_message_by_path(eml_path):
     """
 
     :type eml_path: str
@@ -40,112 +113,92 @@ def parsing_file(eml_path):
         return msg
 
 
-def parse_images_and_save(message, folder_path, relative_image_path):
+def parse_images_and_save(message, folder_for_save, folder_image_name):
     img_id_and_path = {}
 
-    folder_to_image = os.path.join(folder_path, relative_image_path)
+    folder_to_image = os.path.join(folder_for_save, folder_image_name)
     create_folder_or_pass_if_created(folder_to_image)
 
     for part in message.walk():
         if part.get_content_type() == 'image/png' and part["Content-ID"]:  # игнорируем изображения-вложения
-            _, relative_path = write_part(part, folder_path, folder_to_image)
+            _, relative_path = write_part(part, folder_for_save, folder_to_image)
             img_id_and_path[part["Content-ID"][1:-1]] = relative_path  # Для замены в html
 
     logging.debug('Parse and save {} images'.format(len(img_id_and_path)))
     return img_id_and_path
 
 
-def write_part(part, work_directory, folder_to_save):
-    file_name = part.get_filename()
-
-    absolute_path = os.path.join(folder_to_save, file_name)
-    relative_path = os.path.relpath(absolute_path, work_directory)  # Для связи c html
-
-    with open(absolute_path, 'wb') as fb:
-        fb.write(part.get_payload(decode=True))
-
-    logging.debug('File "{}" was recorded'.format(file_name))
-    return file_name, relative_path
-
-
-def parse_attachment_and_save(message, folder_path, relative_attachment_path):
+def parse_attachment_and_save(message, folder_for_save, folder_attachment_name):
     attach_name_and_path = {}
 
-    folder_to_attachment = os.path.join(folder_path, relative_attachment_path)
+    folder_to_attachment = os.path.join(folder_for_save, folder_attachment_name)
     create_folder_or_pass_if_created(folder_to_attachment)
+
     for part in message.iter_attachments():
-        if part.get_content_type() == 'application/octet-stream':  # вложение
-            name, relative_path = write_part(part, folder_path, folder_to_attachment)
+        if part.get_content_type() == 'application/octet-stream':  # Вложения
+            name, relative_path = write_part(part, folder_for_save, folder_to_attachment)
+            if name.endswith('.eml'):
+                abs_path = os.path.join(folder_to_attachment, name)
+                convert_eml_to_html(abs_path)
             attach_name_and_path[name] = relative_path
 
         elif part.get_content_type() == 'image/png' and part["Content-ID"] is None:  # Вложенное изображение
-            name, relative_path = write_part(part, folder_path, folder_to_attachment)
+            name, relative_path = write_part(part, folder_for_save, folder_to_attachment)
             attach_name_and_path[name] = relative_path
 
     logging.debug('Parse and save {} attachments'.format(len(attach_name_and_path)))
     return attach_name_and_path
 
 
-def create_folder_or_pass_if_created(folder_path):
-    try:
-        os.mkdir(folder_path)
-        logging.debug('Create folder "{}"'.format(folder_path))
-    except FileExistsError:
-        pass
-
-
-def parse_html(message):
+def get_html(message):
     for part in message.walk():
         if part.get_content_type() == 'text/html':
-            my_html = part.get_payload(decode=True)
+            html = part.get_payload(decode=True)
 
             logging.debug('Get html')
-            return my_html
+            return html
+
+    raise Exception('Html not found')
 
 
-def replace_link_to_image(html, images_id_and_path):
-    for img_id, img_path in images_id_and_path.items():
-        html = html.replace(bytes('cid:' + img_id, 'utf-8'), bytes(img_path, 'utf-8'))
-
-    logging.debug('Replace link to image')
-    return html
-
-
-def create_list(title, lines, sign='numbers'):
-    added_lines = [b'<DIV><FONT size=4 face=Arial>' + title.encode() + b'</FONT></DIV>',
-                   b'<ol>' if sign == 'numbers' else b'<ul>']
-
-    for line in lines:
-        added_lines.append(b'	<li>' + line.replace('<', '&lt;').encode('koi8-r') + b'</li>')  # &lt; - Обертка '<'
-
-    added_lines.extend([b'</ol>' if sign == 'numbers' else b'</ul>',
-                        b'<HR>'])  # горизонтальная линия
-
-    return added_lines
-
-
-def add_to_top(html, added_lines):
-    lines = html.split(b'\n')
-    number_line_start_with = next(i for i, line in enumerate(lines) if line.startswith(b'<BODY'))
-    result = lines[:number_line_start_with + 1] + added_lines + lines[number_line_start_with + 1:]
-    logging.debug('Add {} lines to top'.format(len(added_lines)))
-    return b'\n'.join(result)
-
-
+# ###################### Main logic #########################################
 def change_html(html, images_id_and_path, attachments, info_about_letters):
     """
 
-    :param html:
-    :type html:
-    :param images_id_and_path:
-    :type images_id_and_path:
-    :param attachments:
-    :type attachments:
-    :param info_about_letters:
-    :type info_about_letters:
-    :return:
-    :rtype:
+    :type html: bytes
+    :param images_id_and_path: словарь из cid сообщений и относительных путей к нему, для замены в html
+    :type images_id_and_path: dict[str, str]
+    :type attachments: dict or list or set
+    :type info_about_letters: list
+    :rtype: bytes
     """
+
+    def replace_link_to_image(html_, images_id_and_path_):
+        for img_id, img_path in images_id_and_path_.items():
+            html_ = html_.replace(bytes('cid:' + img_id, 'utf-8'), bytes(img_path, 'utf-8'))
+
+        logging.debug('Replace link to image')
+        return html_
+
+    def create_list(title, lines, sign='numbers'):
+        added_lines = [b'<DIV><FONT size=4 face=Arial>' + title.encode() + b'</FONT></DIV>',
+                       b'<ol>' if sign == 'numbers' else b'<ul>']
+
+        for line in lines:
+            added_lines.append(
+                b'	<li>' + line.replace('<', '&lt;').encode('koi8-r') + b'</li>')  # &lt; - Обертка '<'
+
+        added_lines.extend([b'</ol>' if sign == 'numbers' else b'</ul>',
+                            b'<HR>'])  # горизонтальная линия
+
+        return added_lines
+
+    def add_to_top(html_, added_lines):
+        lines = html_.split(b'\n')
+        number_line_start_with = next(i for i, line in enumerate(lines) if line.lower().startswith(b'<body'))
+        result = lines[:number_line_start_with + 1] + added_lines + lines[number_line_start_with + 1:]
+        logging.debug('Add {} lines to top'.format(len(added_lines)))
+        return b'\n'.join(result)
 
     if images_id_and_path:
         html = replace_link_to_image(html, images_id_and_path)
@@ -161,16 +214,12 @@ def change_html(html, images_id_and_path, attachments, info_about_letters):
     return html
 
 
-def write_html(html, folder_path, name):
-    html_path = os.path.join(folder_path, name)
-    with open(html_path, 'wb') as f:
-        f.write(html)
+def convert_eml_to_html(eml_path):
+    msg = get_email_message_by_path(eml_path)
 
-    return html_path
+    folder_name = create_name_for_folder(msg['Date'], msg['Subject'])
+    folder_for_save = get_or_create_folder_for_save(eml_path, PATH_OUT, folder_name)
 
-
-def convert_eml_to_html(eml_file, out_folder):
-    msg = parsing_file(eml_file)
     info_about_letters = ['Subject: ' + msg.get("Subject", ""),
                           'Date: ' + msg.get("Date", ""),
                           'From: ' + msg.get("From", ""),
@@ -179,36 +228,36 @@ def convert_eml_to_html(eml_file, out_folder):
                           'Priority: ' + msg.get("X-MSMail-Priority", ""),
                           ]
 
-    folder_name = create_name_for_folder(msg['Date'], msg['Subject'])
-    folder_path = os.path.join(out_folder, folder_name)
-    create_folder_or_pass_if_created(folder_path)
+    images_id_and_path = parse_images_and_save(msg, folder_for_save, PATH_IMAGE)
+    attachments = parse_attachment_and_save(msg, folder_for_save, PATH_ATTACH)
 
-    images_id_and_path = parse_images_and_save(msg, folder_path, PATH_IMAGE)
-    attachments = parse_attachment_and_save(msg, folder_path, PATH_ATTACH)
-
-    html = parse_html(msg)
+    html = get_html(msg)
     html = change_html(html, images_id_and_path, attachments, info_about_letters)
-    html_path = write_html(html, folder_path, name=msg['Subject'] + '.html')
 
-    logging.debug('OK. Convert eml to html [{} --> {}]'.format(eml_file, html_path))
-    return folder_path, html_path
+    html_name = msg['Subject'] + '.html'
+    html_path = write_html(html, folder_for_save, name=html_name)
+
+    shutil.copy2(eml_path, folder_for_save)  # копируем исходных eml в папку. Мб переместить?
+
+    logging.info('OK. Convert eml to html [{} --> {}]'.format(eml_path, html_path))
 
 
 def main():
     eml_files = glob.glob(PATH + '*.eml')  # get all .eml files in a list
-    out_folder = os.path.join(PATH, PATH_OUT)
-    create_folder_or_pass_if_created(out_folder)
-
     for eml_file in eml_files:
-        file_folder, html_path = convert_eml_to_html(eml_file, out_folder)
-        shutil.copy2(eml_file, file_folder)
+        convert_eml_to_html(eml_file)
 
     logging.info('Convert {} eml files to html'.format(len(eml_files)))
 
 
+# ###################### Start ##############################################
 if __name__ == '__main__':
     logging.basicConfig(
         filename=None, level=logging.DEBUG,
         format='[%(asctime)s] %(levelname).1s: %(message)s',
         datefmt='%H:%M:%S')
-    main()
+    try:
+        main()
+    except Exception as err:
+        logging.error(err)
+        raise
