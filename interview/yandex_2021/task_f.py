ea8e538e3517eda125dd50c37c6dd6582a4580a4
appolimp <appolimp@mail.ru>
2021-06-30 13:04:37 +0300

    Update task_f.py

--- interview/yandex_2021/task_f.py
+++ interview/yandex_2021/task_f.py
@@ -2,13 +2,7 @@
 Task F. Спутниковая съёмка
 """
 
-
-def read_input(path):
-    with open(path) as f:
-        _ = next(f)
-        snapshots = [[int(i) for i in row.split()] for row in f]
-
-    return snapshots
+from collections import Counter
 
 
 def write_answer(path, data):
@@ -16,34 +10,25 @@ def write_answer(path, data):
         f.write('\n'.join(str(i) for i in data))
 
 
-def get_left_corners_from_snapshots(snapshot):
-    x1, y1, x2, y2 = snapshot
-
-    if x1 == x2 or y1 == y2:
-        return set()
-
-    left_corners = set()
-    for dx in range(x2 - x1):
-        for dy in range(y2 - y1):
-            left_corners.add((x1+dx, y1+dy))
-
-    return left_corners
-
-
 def main():
-    snapshots = read_input('input.txt')
 
-    actual_photo = set()
-    count_photo_per_days = []
+    left_corners_and_days = {}
 
-    for snapshot in snapshots[::-1]:
-        photo = get_left_corners_from_snapshots(snapshot)
-        actual_photo.update(photo)
-        count_photo_per_days.append(len(actual_photo))
+    with open('input.txt') as f:
+        n = int(next(f))
+        snapshots = ([int(i) for i in row.split()] for row in f)
 
-    count_new_photo_per_day = [count_photo_per_days[0]] + [now - last for now, last in zip(count_photo_per_days[1:],
-                                                                                           count_photo_per_days)]
-    write_answer('output.txt', count_new_photo_per_day[::-1])
+        for day, (x1, y1, x2, y2) in enumerate(snapshots):
+            if x1 == x2 or y1 == y2:
+                continue
+
+            left_corners = ((x1+dx, y1+dy) for dx in range(x2 - x1) for dy in range(y2 - y1))
+            left_corners_and_days.update(dict.fromkeys(left_corners, day))
+
+    days = Counter(left_corners_and_days.values())
+    block_count = (days.get(i, 0) for i in range(n))
+
+    write_answer('output.txt', block_count)
 
 
 if __name__ == '__main__':
